243b6266bc7b2486769f1d90db140929aefcf411
vctrhugoop <vhdestroy@hotmail.com>
2023-11-05 20:15:00 -0300

    feat: add scroll reavel

--- script.js
+++ script.js
@@ -7,3 +7,62 @@ toggleMenu.onclick = () => {
   hidden.classList.toggle('show');
   navMenu.classList.toggle('show');
 };
+
+window.sr = ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.scroll-1', {
+  delay: 100,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0,
+  },
+});
+ScrollReveal().reveal('.scroll-2', {
+  delay: 100,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0,
+  },
+});
+ScrollReveal().reveal('.scroll-3', {
+  delay: 100,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0,
+  },
+});
+ScrollReveal().reveal('.scroll-4', {
+  delay: 100,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0,
+  },
+});
+ScrollReveal().reveal('.scroll-5', {
+  delay: 100,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0,
+  },
+});
+ScrollReveal().reveal('.scroll-6', {
+  delay: 100,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0,
+  },
+});
+ScrollReveal().reveal('.scroll-7', {
+  delay: 100,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0,
+  },
+});
