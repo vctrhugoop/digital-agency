370a4dede4b7f22b4fe8f4a12852e3695ae8e208
vctrhugoop <vhdestroy@hotmail.com>
2023-11-07 18:56:45 -0300

    feat: add anchor links html and script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const toggleMenu = document.querySelector('#toggle-menu');
 const navMenu = document.querySelector('.navbar-wrapper');
 const hidden = document.querySelector('body');
+const links = document.querySelectorAll('nav ul li a');
 
 toggleMenu.onclick = () => {
   toggleMenu.classList.toggle('bx-x');
@@ -8,61 +9,34 @@ toggleMenu.onclick = () => {
   navMenu.classList.toggle('show');
 };
 
+for (const link of links) {
+  link.addEventListener('click', () => {
+    toggleMenu.classList.remove('bx-x');
+    hidden.classList.remove('show');
+    navMenu.classList.remove('show');
+  });
+}
+
 window.sr = ScrollReveal({ reset: true });
 
 ScrollReveal().reveal('.scroll-1', {
   delay: 100,
-  rotate: {
-    x: 100,
-    y: 0,
-    z: 0,
-  },
 });
 ScrollReveal().reveal('.scroll-2', {
   delay: 100,
-  rotate: {
-    x: 100,
-    y: 0,
-    z: 0,
-  },
 });
 ScrollReveal().reveal('.scroll-3', {
   delay: 100,
-  rotate: {
-    x: 100,
-    y: 0,
-    z: 0,
-  },
 });
 ScrollReveal().reveal('.scroll-4', {
   delay: 100,
-  rotate: {
-    x: 100,
-    y: 0,
-    z: 0,
-  },
 });
 ScrollReveal().reveal('.scroll-5', {
   delay: 100,
-  rotate: {
-    x: 100,
-    y: 0,
-    z: 0,
-  },
 });
 ScrollReveal().reveal('.scroll-6', {
   delay: 100,
-  rotate: {
-    x: 100,
-    y: 0,
-    z: 0,
-  },
 });
 ScrollReveal().reveal('.scroll-7', {
   delay: 100,
-  rotate: {
-    x: 100,
-    y: 0,
-    z: 0,
-  },
 });
