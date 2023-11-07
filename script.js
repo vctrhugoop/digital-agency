09b2a3f63c5f9179e20ac8d3411327656ee6f599
vctrhugoop <vhdestroy@hotmail.com>
2023-11-07 19:14:25 -0300

    feat: add button scroll to top

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const toggleMenu = document.querySelector('#toggle-menu');
 const navMenu = document.querySelector('.navbar-wrapper');
 const hidden = document.querySelector('body');
 const links = document.querySelectorAll('nav ul li a');
+const btnToTop = document.getElementById('btn-to-top');
 
 toggleMenu.onclick = () => {
   toggleMenu.classList.toggle('bx-x');
@@ -14,6 +15,25 @@ for (const link of links) {
     toggleMenu.classList.remove('bx-x');
     hidden.classList.remove('show');
     navMenu.classList.remove('show');
+  });
+}
+
+window.onscroll = () => {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnToTop.style.display = 'block';
+  } else {
+    btnToTop.style.display = 'none';
+  }
+}
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
   });
 }
 
